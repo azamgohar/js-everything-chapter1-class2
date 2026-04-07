62ce9e0b2820a98237798778cdad1f393f3035ef
azamgohar <144214140+azamgohar@users.noreply.github.com>
2026-04-07 22:15:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Banking Example
+let balance = 15000;
+
+console.log("ghadi lene se pehle ka balance", balance);
+let watchPrice = 2500;
+balance = balance-watchPrice;
+console.log("ghadi lene ke baad ka balance", balance);
+
+// Snake Game Example:
+
+let snakeLength = 1;
+
+let apple = 5;
+
+snakeLength = snakeLength+apple;
+console.log("Total Length is", snakeLength);
+snakeLength = snakeLength+apple;
+console.log("Total Length is", snakeLength);
+console.log("Game Over");
